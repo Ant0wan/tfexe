c4d007d3fc6f38c2444caf8c3e684dade2494792
Antoine Barthelemy <antoine@linux.com>
2023-04-20 18:21:36 +0200

    install script

--- install.sh
+++ install.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
-curl $(curl -s https://api.github.com/repos/Ant0wan/tfexe/releases/latest | grep browser_download_url | awk -F '": ' '{print $2}' | xargs echo) -o tfexe
+wget "$(wget --output-document - https://api.github.com/repos/Ant0wan/tfexe/releases/latest 2>/dev/null | grep browser_download_url | awk -F '": ' '{print $2}' | xargs echo)"
 sudo install tfexe /usr/local/bin
+rm tfexe
