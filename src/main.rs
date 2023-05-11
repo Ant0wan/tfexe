230a9c82ebc2f11c8f2fbf38ebc0c438d28f77c4
Antoine Barthelemy <antoine@linux.com>
2023-05-11 20:47:57 +0200

    version ok

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,30 @@
 use std::env;
 use std::io::Result;
+use std::process::exit;
 use std::process::Command;
 
 fn main() -> Result<()> {
     let args: Vec<String> = env::args().skip(1).collect();
 
-    let _ = Command::new("/usr/local/bin/tfswitch")
+    let tfswitch = Command::new("tfswitch")
         .status()
-        .expect("failed to get terraform binary of the specified version");
+        .expect("failed to execute tfswitch");
 
-    let status = Command::new("terraform")
+    let tfswitch_exit_value = tfswitch.code().unwrap_or(1);
+    if tfswitch_exit_value != 0 {
+        exit(tfswitch_exit_value);
+    }
+
+    let terraform = Command::new("terraform")
         .args(args)
         .status()
         .expect("failed to execute terraform");
 
-    if status.success() && status.success() {
-        Ok(())
-    } else {
-        Err(std::io::Error::new(
-            std::io::ErrorKind::Other,
-            "Command failed",
-        ))
+    let terraform_exit_value = terraform.code().unwrap_or(1);
+
+    if terraform_exit_value != 0 {
+        exit(terraform_exit_value);
     }
+
+    Ok(())
 }
