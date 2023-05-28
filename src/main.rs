fd4d5b6f3bd586289d9d12027adbc2fdfde7dadf
Antoine Barthelemy <antoine@linux.com>
2023-05-28 13:51:13 +0200

    add strong types

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,18 @@
 use std::env;
 use std::io::Result;
-use std::process::{exit, Command};
+use std::process::{exit, Child, Command, ExitStatus};
 
 fn main() -> Result<()> {
     let args: Vec<String> = env::args().skip(1).collect();
 
-    let mut tfswitch = Command::new("tfswitch").spawn()?;
-    let tfswitch_exit_status = tfswitch.wait()?;
+    let mut tfswitch: Child = Command::new("tfswitch").spawn()?;
+    let tfswitch_exit_status: ExitStatus = tfswitch.wait()?;
     if !tfswitch_exit_status.success() {
         exit(tfswitch_exit_status.code().unwrap_or(1));
     }
 
-    let mut terraform = Command::new("terraform").args(args).spawn()?;
-    let terraform_exit_status = terraform.wait()?;
+    let mut terraform: Child = Command::new("terraform").args(args).spawn()?;
+    let terraform_exit_status: ExitStatus = terraform.wait()?;
     if !terraform_exit_status.success() {
         exit(terraform_exit_status.code().unwrap_or(1));
     }
